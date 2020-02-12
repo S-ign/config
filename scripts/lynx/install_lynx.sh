5cf973a53a36838441813cb75c54c98c64224e5d
Sign <avery.carty7@gmail.com>
2020-02-12 01:27:03 -0500

    updated scripts/lynx

--- scripts/lynx/install_lynx.sh
+++ scripts/lynx/install_lynx.sh
@@ -3,7 +3,8 @@
 #credit goes to rwxrob for this setup! Thank you dude!
 #Of course also thanks for the creaters of lynx!
 
+d=$(pwd)
+
 sudo apt-get install lynx
-~/lynx/appendtobashrc >> ~/.bashrc
-mv ~/lynx/.lynx.cfg ~/lynx/.lynx.lss ~/
-rm -rf ~/lynx
+cat $d/appendtobashrc >> ~/.bashrc
+cp  $d/.lynx.cfg $d/.lynx.lss ~/
