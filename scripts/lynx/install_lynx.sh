2c6f8eb78147924186afaefe03f8bb2c27348dfa
Sign <avery.carty7@gmail.com>
2020-02-12 00:51:26 -0500

    added lynx installation files

---
+++ scripts/lynx/install_lynx.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#credit goes to rwxrob for this setup! Thank you dude!
+#Of course also thanks for the creaters of lynx!
+
+sudo apt-get install lynx
+~/lynx/appendtobashrc >> ~/.bashrc
+mv ~/lynx/.lynx.cfg ~/lynx/.lynx.lss ~/
